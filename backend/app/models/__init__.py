5cf7c7f9cf167da4cafec808f22d7c9206861ec1
ShreyMangle <shreymangle2004@gmail.com>
2026-01-31 09:56:49 +0530

    added database session handling and initialised the tables

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,2 @@
+from backend.app.models.college import College
+from backend.app.models.cutoff import Cutoff
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from backend.app.core.database import Base, engine
 
 app=FastAPI(
     title="First Fly",
     version="0.1.0",
 )
+
+Base.metadata.create_all(bind=engine)
